5d38b16d866df1b223f6f381dd061c682b714cbb
noor alam <51040978+noorshanu@users.noreply.github.com>
2022-10-20 23:52:10 +0530

    gvhcv

--- src/Component/Nft/Nft.js
+++ src/Component/Nft/Nft.js
@@ -1,18 +1,30 @@
 import React from 'react'
 import './nft.css'
-
+import Tshirt from '../../assets/tshirt.png'
 function Nft() {
   return (
     <section className='nft'>
-        <div className='container text-center nft-head'>
+    <div className='container text-center nft-head'>
         <h1>NFTs</h1>
         <p>At MetaBall, NFTs are at the centre of everything.</p>
-        </div>
-    <div class="container">
+    </div>
+    <div class="container nft-box">
         <div class="row justify-content-center align-items-center g-2">
-            <div class="col">Column</div>
-            <div class="col">Column</div>
-            <div class="col">Column</div>
+        <div class="col-md-6 nft-para">
+                <p> The NFT Marketplace has unique characters, footballs, kits, bands and other 
+                    football accessories. Every NFT is one of a kind and has its own distinct appearance,
+                    personality, and energy. </p>
+                <p>One NFT cannot be reproduced or shared on chain with another NFT under any circumstances.
+                     One NFT is equal to one vote.</p>
+                <p>After the collection has been released to the public, our very own marketplace for NFTs will go
+                     online. Here, players will be able to buy, sell, and trade NFTs in order to acquire the greatest possible 
+                     copy of the game.</p>
+                </div>
+            <div class="col-md-6 text-center nft-slider">
+                <img src={Tshirt} alt=""/>
+
+            </div>
+            
         </div>
     </div>    
     </section>
