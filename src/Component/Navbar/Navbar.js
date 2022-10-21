b7d84481726761c37e13b18845b5b8c62269f2e1
noor alam <51040978+noorshanu@users.noreply.github.com>
2022-10-21 11:15:24 +0530

    nav

--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -1,19 +1,36 @@
-import React from 'react'
+import React, { useState } from "react";
 import './Navbar.css'
+import { GiHamburgerMenu } from "react-icons/gi";
+import Logo from '../../assets/Logo.png'
 
 function Navbar() {
+  const [showMediaIcons, setShowMediaIcons] = useState(false);
   return (
     <nav className='container main-nav'>
-    <div className='logo'>logo</div>
-    <div className='menu-link'>
+    <div className='logo'>   <div className='d-flex foot-logo'>
+            <img src={Logo} alt="" className=''/>
+            <h2 className='foot-h1'>METABALL</h2>
+            </div></div>
+    <div className={
+            showMediaIcons ? "menu-link mobile-menu-link" : "menu-link"
+          }>
     <ul>
-        <li>home</li>
-        <li>about</li>
-        <li>home</li>
-        <li>about</li>
+        <li><a href="/">About</a></li>
+        <li><a href="/">Features</a></li>
+        <li><a href="/">Gameplay</a></li>
+        <li><a href="/">Tokenomics</a></li>
+        <li><a href="/">NFTs</a></li>
+        <li><a href="/">Roadmap</a></li>
+        <li><a href="/">team</a></li>
     </ul>
 
     </div>
+      {/* hamburget menu start  */}
+      <div className="hamburger-menu">
+            <a href="#home" onClick={() => setShowMediaIcons(!showMediaIcons)}>
+              <GiHamburgerMenu />
+            </a>
+          </div>
     </nav>
   )
 }
