0083594c81f772bac58f9b64a1ab170dc309ddd7
noor alam <51040978+noorshanu@users.noreply.github.com>
2022-11-10 16:58:06 +0530

    gffh

--- src/Component/Features/Features.js
+++ src/Component/Features/Features.js
@@ -1,16 +1,80 @@
-import React from 'react'
-
+import React from "react";
+import "./feature.css";
+import Blank from "../../assets/blnkpic.png";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 function Features() {
   return (
-    <section className='features' id='feature'>
+    <section className="features" id="feature">
       <div class="container text-center">
-       <h1>Features</h1>
-
-       <h1>soon....</h1>
+        <h1>Teams</h1>
       </div>
 
+      <div className="container">
+        <Tabs>
+          <TabList>
+            <Tab>
+              <p>Title 1</p>
+            </Tab>
+            <Tab>
+              <p>Title 2</p>
+            </Tab>
+            <Tab>
+              <p>Title 3</p>
+            </Tab>
+          </TabList>
+
+          <TabPanel>
+            <div className="panel-content">
+              <div className="d-flex">
+                <div className="tab-content">
+                  <p>
+                    The MetaBall Utility token stores your values and grants you
+                    access to the unlimited elements in the MetaBall Football
+                    Game.
+                  </p>
+                </div>
+                <div className="tab-image">
+                  <img src={Blank} alt="" />
+                </div>
+              </div>
+            </div>
+          </TabPanel>
+          <TabPanel>
+            <div className="panel-content">
+            <div className="d-flex">
+                <div className="tab-content">
+                  <p>
+                    The MetaBall Utility token stores your values and grants you
+                    access to the unlimited elements in the MetaBall Football
+                    Game.
+                  </p>
+                </div>
+                <div className="tab-image">
+                  <img src={Blank} alt="" />
+                </div>
+              </div>
+            </div>
+          </TabPanel>
+          <TabPanel>
+            <div className="panel-content">
+            <div className="d-flex">
+                <div className="tab-content">
+                  <p>
+                    The MetaBall Utility token stores your values and grants you
+                    access to the unlimited elements in the MetaBall Football
+                    Game.
+                  </p>
+                </div>
+                <div className="tab-image">
+                  <img src={Blank} alt="" />
+                </div>
+              </div>
+            </div>
+          </TabPanel>
+        </Tabs>
+      </div>
     </section>
-  )
+  );
 }
 
-export default Features
+export default Features;
--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -1,9 +1,32 @@
 import React, { useState } from "react";
+import Web3Modal from 'web3modal';
 import './Navbar.css'
 import { GiHamburgerMenu } from "react-icons/gi";
 import Logo from '../../assets/Logo.png'
 
+import {ethers} from 'ethers'
+
+const providerOption ={
+
+}
+
 function Navbar() {
+  async function connectWallet(){
+    try{
+      let web3Modal=new Web3Modal( {
+        cacheProvider:false,
+        providerOption,
+
+      })
+      const web3ModalInstance =await web3Modal.connect();
+      const web3ModalProvider =new ethers.providers.Web3Provider(web3ModalInstance);
+      console.log(web3ModalProvider)
+    }catch(error){
+      console.error(error);
+
+    }
+
+  }
   const [showMediaIcons, setShowMediaIcons] = useState(false);
   return (
     <nav className='container main-nav'>
@@ -22,6 +45,7 @@ function Navbar() {
         <li><a href="#nft">NFTs</a></li>
         <li><a href="#road">Roadmap</a></li>
         <li><a href="#team">team</a></li>
+        <li><button onClick={connectWallet}>connect</button></li>
     </ul>
 
     </div>
