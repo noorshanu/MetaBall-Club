bf6cf623952a44df715a11107f93faf3182235d6
noor alam <51040978+noorshanu@users.noreply.github.com>
2022-11-13 13:57:22 +0530

    dem

--- src/Component/Hero/Hero.js
+++ src/Component/Hero/Hero.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import './Hero.css'
 import {FaTwitter,FaTelegram,FaLinkedin,FaGithub} from 'react-icons/fa'
-
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation,EffectFade, Autoplay } from "swiper";
 function Hero() {
   return (
     <section className='Hero-main'>
-        <div className='container'>
+   <Swiper
+            navigation={true}
+            effect={"fade"}
+            autoplay={{
+              delay: 2500,
+              disableOnInteraction: false,
+            }}
+            modules={[EffectFade,Navigation, Autoplay]}
+            className="mySwiper"
+          >
+            <SwiperSlide>
+            <div className='bg1'>
+            <div className='container'>
             <div className='row'>
                 <div className='col-md-6  align-items-center'>
                     <h1 className='pt-2 hero-h1'>
@@ -33,6 +48,43 @@ function Hero() {
             </div>
 
         </div>
+        </div>
+            </SwiperSlide>
+            <SwiperSlide>
+                <div className='bg2'>
+            <div className='container'>
+            <div className='row'>
+                <div className='col-md-6  align-items-center'>
+                    <h1 className='pt-2 hero-h1'>
+                    Where the Players Want to 2
+                    </h1>
+                    <span className='hero-btn'> <a href="/" className='hero-btn-1'>play</a></span>
+                   
+
+                    <div>
+                    <p className='hero-p'>Connect With The Amazing Community</p>
+                    <div className='hero-social'>
+                        <a href="/"><FaTwitter/></a>
+                        <a href="/"><FaTelegram/></a>
+                        <a href="/"><FaLinkedin/></a>
+                        <a href="/"><FaGithub/></a>
+
+                    </div>
+                    </div>
+
+                </div>
+                <div className='col-md-6'>
+                    
+                </div>
+
+            </div>
+
+        </div>
+        </div>
+            </SwiperSlide>
+            </Swiper>
+
+
     </section>
   )
 }
