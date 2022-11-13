b2ccbe7b744ee15d5155877a464371ff3bdfc3c6
noor alam <51040978+noorshanu@users.noreply.github.com>
2022-11-14 00:28:12 +0530

    jo

--- src/Component/About/About.js
+++ src/Component/About/About.js
@@ -1,64 +1,86 @@
 import React, { Suspense } from "react";
-import './About.css'
+import "./About.css";
 // import AboutImg from '../../assets/blnkpic.png'
 import { Canvas } from "react-three-fiber";
 import { OrbitControls, useGLTF } from "@react-three/drei";
 function Model(props) {
-  const { scene } = useGLTF('art2.glb');
+  const { scene } = useGLTF("art2.glb");
   return <primitive object={scene} />;
 }
 function About() {
   return (
-    <section className='about-main' id='about'>
-    <div className='container'>
-        <div className='row'>
-            <div className='col-md-6'>
-                <div className='about-img'>
-                <Canvas pixelRatio={[1, 2]} camera={{ position: [-15, 10, 40], fov: 60 }} style={{width:'100%',height:'450px'}} >
-      <ambientLight intensity={10} />
-      <Suspense fallback={null}>
-        <Model />
-      </Suspense>
-      <OrbitControls />
-    </Canvas>
-
-                </div>
+    <section className="about-main" id="about">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <div className="about-img">
+              <Canvas
+                pixelRatio={[1, 2]}
+                camera={{ position: [-30, 70, 80], fov: 80 }}
+                style={{
+                  width: "100%",
+                  height: "450px",
+                  border: "1px solid #FF00D6",
+                  borderRadius: "10px",
+                }}
+              >
+                <ambientLight intensity={10} />
+                <Suspense fallback={null}>
+                  <Model />
+                </Suspense>
+                <OrbitControls />
+              </Canvas>
             </div>
-            <div className='col-md-6'>
-                <div className='about-content'>
-                    <h1>About</h1>
-                    <p>In order to provide a live exposure of the people into the football field the
-                         MetaBall Club presents MetaBall Football Game, where the feeling of real and 
-                         virtual are almost the same. The main motive of the core team behind this project 
-                         is to provide best experience to players in metaverse. </p>
-                    <p>
-                        All the assets in this metaverse game will be in the form of NFT’s and every NFT is
-                         equivalent to one vote. This project will be a DAO game in the metaverse. The 
-                         number of having NFT’s is directly related to votes. The more NFT’s you have the 
-                         more votes you will have and having more NFT, or Votes is related to your strength 
-                         in the project. It means with more votes you can have more strength in the game.
-                         </p>
+          </div>
+          <div className="col-md-6">
+            <div className="about-content">
+              <h1>About</h1>
+              <p>
+                In order to provide a live exposure of the people into the
+                football field the MetaBall Club presents MetaBall Football
+                Game, where the feeling of real and virtual are almost the same.
+                The main motive of the core team behind this project is to
+                provide best experience to players in metaverse.{" "}
+              </p>
+              <p>
+                All the assets in this metaverse game will be in the form of
+                NFT’s and every NFT is equivalent to one vote. This project will
+                be a DAO game in the metaverse. The number of having NFT’s is
+                directly related to votes. The more NFT’s you have the more
+                votes you will have and having more NFT, or Votes is related to
+                your strength in the project. It means with more votes you can
+                have more strength in the game.
+              </p>
 
-                      <p> This project is based on Avalance, BSC, Etherium, and Polygon. The advertisements in the
-                         metaverse will be active advertisements and the revenue generated from them can be used 
-                         to buy back MetaBall tokens. The native token of this MetaBall Project will be the MetaBall 
-                         Token. </p> 
+              <p>
+                {" "}
+                This project is based on Avalance, BSC, Etherium, and Polygon.
+                The advertisements in the metaverse will be active
+                advertisements and the revenue generated from them can be used
+                to buy back MetaBall tokens. The native token of this MetaBall
+                Project will be the MetaBall Token.{" "}
+              </p>
 
-                      <p> All the transactions within this project will be made in the native token.</p>  
+              <p>
+                {" "}
+                All the transactions within this project will be made in the
+                native token.
+              </p>
 
-                      <div className='about-btn-box'>
-                      <span className='hero-btn'> <a href="/" className='hero-btn-1'>play</a></span>
-                        </div>    
-
-                </div>
-
+              <div className="about-btn-box">
+                <span className="hero-btn">
+                  {" "}
+                  <a href="/" className="hero-btn-1">
+                    play
+                  </a>
+                </span>
+              </div>
             </div>
-
+          </div>
         </div>
-        
-    </div>
+      </div>
     </section>
-  )
+  );
 }
 
-export default About
+export default About;
