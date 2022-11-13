c22c57400ecfcd937d4dc1b3cb0b26dc35d07c3d
noor alam <51040978+noorshanu@users.noreply.github.com>
2022-11-13 14:48:58 +0530

    fgh

--- src/Component/Hero/Hero.js
+++ src/Component/Hero/Hero.js
@@ -82,6 +82,39 @@ function Hero() {
         </div>
         </div>
             </SwiperSlide>
+
+            <SwiperSlide>
+                <div className='bg3'>
+            <div className='container'>
+            <div className='row'>
+                <div className='col-md-6  align-items-center'>
+                    <h1 className='pt-2 hero-h1'>
+                    Where the Players Want to 2
+                    </h1>
+                    <span className='hero-btn'> <a href="/" className='hero-btn-1'>play</a></span>
+                   
+
+                    <div>
+                    <p className='hero-p'>Connect With The Amazing Community</p>
+                    <div className='hero-social'>
+                        <a href="/"><FaTwitter/></a>
+                        <a href="/"><FaTelegram/></a>
+                        <a href="/"><FaLinkedin/></a>
+                        <a href="/"><FaGithub/></a>
+
+                    </div>
+                    </div>
+
+                </div>
+                <div className='col-md-6'>
+                    
+                </div>
+
+            </div>
+
+        </div>
+        </div>
+            </SwiperSlide>
             </Swiper>
 
 
